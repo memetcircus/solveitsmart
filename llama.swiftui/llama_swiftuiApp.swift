85107cc1ee55c0e840689cc5557a79b86f0967b6
M. Akif Acar <mehmetakifacar@gmail.com>
2025-08-06 18:18:04 +0300

    Update llama_swiftuiApp.swift
    remove commented codes

--- llama.swiftui/llama_swiftuiApp.swift
+++ llama.swiftui/llama_swiftuiApp.swift
@@ -1,29 +1,3 @@
-/*import SwiftUI
-
-@main
-struct llama_swiftuiApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}*/
-
-/*
-import SwiftUI
-
-@main
-struct LlamaSwiftUIApp: App {
-    @StateObject private var store = ProblemStore()  // Step 1: Create and store your ProblemStore
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environmentObject(store)  // Step 2: Inject into environment
-        }
-    }
-}*/
-
 import SwiftUI
 
 @main
